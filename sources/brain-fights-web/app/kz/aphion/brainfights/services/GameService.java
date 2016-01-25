daff2cfcef9021ee4b9e282d14891448bd57a1c6
Alimzhan Nurpeissov <alimzhan.nurpeissov@bee.kz>
2016-01-26 02:37:26 +0600

    Не правильный подсчет очков

--- sources/brain-fights-web/app/kz/aphion/brainfights/services/GameService.java
+++ sources/brain-fights-web/app/kz/aphion/brainfights/services/GameService.java
@@ -940,12 +940,16 @@ public class GameService {
 			prop = (float) (hiGamerScores*1.00/(hiGamerScores+lowGamerScores));
 		}
 		System.out.println("prop="+prop);
-		double hiScore =  (diff*(((1+Math.signum(diff))/2)-Math.signum(diff)*prop) + (0.5-prop)*(1-Math.abs(Math.signum(diff))));
+		double hiScore =  (diff*(((1.00+Math.signum(diff))/2.00)-Math.signum(diff)*prop) + (0.5-prop)*(1-Math.abs(Math.signum(diff))));
 		System.out.println("hiScore="+hiScore);
 		if(hiScore<0){
 			hiScore=-Math.ceil(-hiScore);
 		}else {
 			hiScore =  Math.ceil(hiScore);
+		}
+		if(hiScore==0&&diff!=0){
+			if(diff>0)hiScore=1;
+			else hiScore=-1;
 		}
 		hiGamer.setScore((int) hiScore);
 		lowGamer.setScore((int) (-hiScore));
