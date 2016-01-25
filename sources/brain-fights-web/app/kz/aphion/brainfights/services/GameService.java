9a64ff97bfc77aabbe28cfb5f725b8c77649caac
Alimzhan Nurpeissov <alimzhan.nurpeissov@bee.kz>
2016-01-26 00:18:38 +0600

    Логгирование подсчета рейтинга

--- sources/brain-fights-web/app/kz/aphion/brainfights/services/GameService.java
+++ sources/brain-fights-web/app/kz/aphion/brainfights/services/GameService.java
@@ -926,15 +926,20 @@ public class GameService {
 			lowGamer = gamer;
 		}
 		int hiGamerScores = hiGamer.getUser().getScore();
+		System.out.println("hiGamerScores="+hiGamerScores);
 		int lowGamerScores = lowGamer.getUser().getScore();
+		System.out.println("lowGamerScores="+lowGamerScores);
 		int diff = hiGamer.getCorrectAnswerCount()-lowGamer.getCorrectAnswerCount();
+		System.out.println("diff="+diff);
 		float prop;
 		if(hiGamerScores+lowGamerScores==0){
 			prop = (float) ((1+hiGamerScores)*1.00/((hiGamerScores+1)+(lowGamerScores+1)));
 		}else{
 			prop = (float) (hiGamerScores*1.00/(hiGamerScores+lowGamerScores));
 		}
+		System.out.println("prop="+prop);
 		double hiScore =  (diff*(((1+Math.signum(diff))/2)-Math.signum(diff)*prop) + (0.5-prop)*(1-Math.abs(Math.signum(diff))));
+		System.out.println("hiScore="+hiScore);
 		if(hiScore<0){
 			hiScore=-Math.ceil(-hiScore);
 		}else {
@@ -944,6 +949,7 @@ public class GameService {
 		lowGamer.setScore((int) (-hiScore));
 		hiGamer.getUser().setScore(hiGamer.getUser().getScore()+hiGamer.getScore());
 		lowGamer.getUser().setScore(lowGamer.getUser().getScore()+lowGamer.getScore());
+		System.out.println("Calculate score "+hiGamer.getUser().getLogin()+":"+lowGamer.getUser().getLogin()+" scores "+hiGamer.getScore()+":"+lowGamer.getScore());
 	}
 	
 
